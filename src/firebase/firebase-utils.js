20a338a2178c1ec7e1567f8055914b98899ce7ed
megurock <e.meguro@gmail.com>
2020-01-26 15:11:27 +0900

    move fetch functionality to reduex. Also implemented container pattern to shop page

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -46,7 +46,7 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit()
 }
 
-export const convertCollectionSnapshotToMap = (collections) => {
+export const convertCollectionsSnapshotToMap = (collections) => {
   const transformedCollection = collections.docs.map(doc => {
     const { title, items } = doc.data()
     return {
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,12 +7,24 @@ export const selectCollections = createSelector(
   shop => shop.collections
 )
 
- export const selectCollectionsForPreview = createSelector(
+export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
+  collections =>
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 )
 
-export const selectCollection = collectionUrlParam => createSelector(
-  [selectCollections],
-  collections => (collections ? collections[collectionUrlParam] : null)
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => (collections ? collections[collectionUrlParam] : null)
+  )
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+)
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
 )
