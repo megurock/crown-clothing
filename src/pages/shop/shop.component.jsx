764fbf30267d1339cf6e69d0c792d7c29363ffe0
megurock <e.meguro@gmail.com>
2020-02-02 12:40:13 +0900

    updated our application App component, sign-in componet and sign-up component to use hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
-class ShopPage extends React.Component {
+const ShopPage = ({ fetchCollectionsStart, match }) => {
 
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props
+  useEffect(() => {
     fetchCollectionsStart()
-  }
+  }, [fetchCollectionsStart])
 
-  render() {
-    const { match } = this.props
-    return (
-      <div className='shop-page'>
-        <Route exact 
-          path={`${match.path}`} 
-          component={CollectionsOverviewContainer}
-        />
-        <Route 
-          path={`${match.path}/:collectionId`} 
-          component={CollectionPageContainer}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className='shop-page'>
+      <Route exact 
+        path={`${match.path}`} 
+        component={CollectionsOverviewContainer}
+      />
+      <Route 
+        path={`${match.path}/:collectionId`} 
+        component={CollectionPageContainer}
+      />
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
